6e6c73a15ee4fa6e5f87cd7961e9ae92d90ada71
Jass <145933956+jasspir@users.noreply.github.com>
2024-06-21 10:46:47 -0400

    Add files via upload

---
+++ src/main/java/me/jass/pluginusingapi/PluginUsingAPI.java
@@ -0,0 +1,38 @@
+package me.jass.pluginusingapi;
+
+import me.jass.api.API;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public final class PluginUsingAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().log(Level.WARNING, "grabbing api...");
+        API api = getServer().getServicesManager().load(API.class);
+
+        if (api == null) {
+            getLogger().log(Level.WARNING, "failed to grab api");
+        } else {
+            getLogger().log(Level.WARNING, api.getName());
+        }
+    }
+
+    public void getAPI() {
+        getLogger().log(Level.WARNING, "grabbing api...");
+        API api = getServer().getServicesManager().load(API.class);
+
+        if (api == null) {
+            getLogger().log(Level.WARNING, "failed to grab api");
+        } else {
+            getLogger().log(Level.WARNING, api.getName());
+        }
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
